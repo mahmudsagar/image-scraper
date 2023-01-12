ae8b907b8219db43c83a5ddd0284401f6f140664
mahmudsagar <mahmudsagar9@gmail.com>
2023-01-13 03:56:26 +0600

    google scraper adde

---
+++ googleScraper.js
@@ -0,0 +1,50 @@
+import puppeteer from "puppeteer";
+
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight - window.innerHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false, timeout: 0, defaultViewport: null })
+    const page = await browser.newPage()
+    // const device_width = 1920;
+    // const device_height = 1080;
+    // await page.setViewport({ width: device_width, height: device_height })
+    await page.goto("https://images.google.com/", { timeout: 0,  })
+    await page.waitForSelector('input', { timeout: 0 });
+    await page.type('input', 'women in burqa')
+    await page.keyboard.press('Enter')
+    await page.waitForNavigation()
+    // await page.waitForSelector('.MSM1fd');
+    await page.waitForSelector('.bRMDJf img');
+    // await autoScroll(page);
+    
+    const options = await page.$$eval('.bRMDJf img', as => as.map(a => a.src));
+    
+    // const element = await page.$$eval('.MSM1fd', as => as.map(a => a.src));
+    
+    
+    for (const el of await page.$$('a.islib')) {
+        // console.log(el);
+        await el.click()
+        await page.waitForSelector('.bRMDJf img');
+        const oneLevel = await page.$$eval('.islir img', as => as.map(a => a.src));
+        console.log(oneLevel);
+    }
+})()
+
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: false, timeout:0 })
     const page = await browser.newPage()
     await page.goto("https://www.pinterest.com/")
     await page.waitForSelector('button.RCK');
@@ -10,8 +10,27 @@ import puppeteer from "puppeteer";
     await page.type('#email', 'mahmudulhasansagarxs@gmail.com')
     await page.type('#password', 'mahmud')
     await page.click('.SignupButton')
-    await page.waitForSelector('#searchBoxContainer');
-    await page.type('#searchBoxContainer input', 'women in burqa');
-    await page.waitForSelector('#SuggestionsMenu');
-    await page.keyboard.press('Enter')
+    const context = browser.defaultBrowserContext();
+    //        URL                  An array of permissions
+    context.overridePermissions("https://www.pinterest.com/", ["geolocation", "notifications"]);
+    page.on("load", async()=>{
+        await page.waitForSelector('#HeaderContent #searchBoxContainer');
+        // await page.waitForNavigation();
+        await page.type('#searchBoxContainer input', 'women in burqa');
+        await page.waitForSelector('#SuggestionsMenu');
+        await page.keyboard.press('Enter')
+    })
+    await page.waitForSelector('.mainContainer .gridCentered')
+    const options = await page.$$eval('.mainContainer .gridCentered a', as => as.map(a => a.href));
+    for (let item of options) {
+        // console.log(item);
+        const page2 = await browser.newPage()
+        await page2.goto(item)
+        await page2.waitForSelector('#gradient [data-test-id="visual-content-container"] img')
+        // const example = await page2.$('[data-test-id="visual-content-container"]');
+        // await page.click('[data-test-id="visual-content-container"] img', {button: 'right'})
+
+        // console.log(example);
+        // page2.close()
+    }
 })()
